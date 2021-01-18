6d1efada03f3ebd837a50852e342d554d3584de0
Alex Picho <64750738+AlexDanielin@users.noreply.github.com>
2021-01-18 16:00:15 -0500

    Add files via upload

---
+++ if.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:53:31 2021
+
+@author: alexp
+"""
+
+datos=10
+nativa=100
+if datos==nativa:
+    print('Las VLAN son iguales')
+else:
+    print('Las VLAN son diferentes')
+    
---
+++ input.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:12:29 2021
+
+@author: alexp
+"""
+
+
+print("RED SOCIO EMPLEO QUITO")
+nombre= input('Ingrese su nombre: ')
+apellido= input('Ingrese su apellido: ')
+ubicacion= input('Ingrese su ubicación: ')
+edad= input('Ingrese su edad: ')
+space=" "
+print('Estimado '+space+nombre+space+apellido+space+ 
+      'al momento no hay vacantes en la ciudad de'+space+ 
+      ubicacion+space+'pese a tener la edad de '+space+edad+space+'años')
